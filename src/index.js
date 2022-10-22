d6b5dcaa5c495be93dc5a53e3037bb9f50d796e7
Stefan-RR <stefan.ruchlewicz@outlook.com>
2022-10-22 16:10:15 -0600

    Combined all coin info into the first table column and made fixed in place it in place on horizontal scroll

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
+// Fetch api data then insert into table
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then((response) =>
     response.json())
     .then((data) => {
         for (let i = 0; i < data.length; i++) {
+            // Generate table structure
             const tableRow = document.getElementById("table").appendChild(document.createElement('tr'))
             tableRow.setAttribute("id", data[i].name);
             tableRow.setAttribute("class", "row");
             const rank = document.getElementById(data[i].name).appendChild(document.createElement('td'))
-            const image = document.getElementById(data[i].name).appendChild(document.createElement('td'))
-            image.setAttribute("class", "image");
-            const imageContent = '<img src='+data[i].image+' class="imageContent"/>'
-            const name = document.getElementById(data[i].name).appendChild(document.createElement('td'))
-            name.setAttribute("class", "name");
-            const symbol = document.getElementById(data[i].name).appendChild(document.createElement('td'))
-            symbol.setAttribute("class", "space"); 
+            rank.setAttribute("class", "coin");
+            const imageContent = '<img src='+ data[i].image +' class="imageContent"/>'
             const price = document.getElementById(data[i].name).appendChild(document.createElement('td'))
             price.setAttribute("class", "num");
             const priceChange = document.getElementById(data[i].name).appendChild(document.createElement('td'))
@@ -22,15 +19,14 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
             volume.setAttribute("class", "num");
             const marketCap = document.getElementById(data[i].name).appendChild(document.createElement('td')) 
             marketCap.setAttribute("class", "num");
-            rank.innerText = data[i].market_cap_rank;
-            image.innerHTML = imageContent;
-            name.innerHTML = data[i].name + "<span class='symbol'>" + data[i].symbol.toUpperCase() +"</span>";
+            // Insert api data into appropriate td
+            rank.innerHTML = "<span class='rank'>" + data[i].market_cap_rank + "</span>" + "<span class='image'>" + imageContent + "</span>" +"<span class='search'>" + "<span class='name'>" + data[i].name + "</span>" + "<span class='symbol'>" + data[i].symbol.toUpperCase() +"</span>" + "</span>";
             price.innerText = '$' + data[i].current_price;
             priceChange.innerText = data[i].price_change_percentage_24h.toFixed(2) + '%'; 
             volume.innerText = '$' + data [i].total_volume.toLocaleString('en-US');
             marketCap.innerText = '$' + data [i].market_cap.toLocaleString('en-US');
-    }
-    }
+            }
+        }
     )
 
     function findCrypto(){
@@ -40,7 +36,7 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
         const tr = table.getElementsByTagName("tr");
     
         for(i = 0; i < tr.length; i++) {
-           const td = tr[i].getElementsByClassName("name")[0];
+           const td = tr[i].getElementsByClassName("search")[0];
             if (td) {
                  const txtValue = td.textContent || td.innerText;
                 if(txtValue.toUpperCase().indexOf(filter) > -1) {
