08fdbb4643e9dd1fac7af60a21a8b37506599e9c
Stefan-RR <stefan.ruchlewicz@outlook.com>
2022-12-09 20:11:20 -0700

    Switched faulty colour change function for working ternary operator

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
             const price = document.getElementById(data[i].name).appendChild(document.createElement('td'))
             price.setAttribute("class", "num");
             const priceChange = document.getElementById(data[i].name).appendChild(document.createElement('td'))
-            priceChange.setAttribute("class", "pnum");
+            priceChange.setAttribute("id", data[i].price_change_percentage_24h);
+            priceChange.setAttribute("class", "num");
             const volume = document.getElementById(data[i].name).appendChild(document.createElement('td'))
             volume.setAttribute("class", "num");
             const marketCap = document.getElementById(data[i].name).appendChild(document.createElement('td')) 
@@ -22,31 +23,18 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
             // Insert api data into appropriate td
             rank.innerHTML = "<div class='coinDiv'>" + "<div class='rankDiv'>" + "<span class='rank'>" + data[i].market_cap_rank + "</span>" + "</div>" + "<div class='imageDiv'>" + "<span class='image'>" + imageContent + "</span>" + "</div>" + "<div class='searchDiv'>" + "<a class='search'>" + "<span class='name'>" + data[i].name + "</span>" + "<span class='symbol'>" + data[i].symbol.toUpperCase() +"</span>" + "</a>" + "</div>" + "</div>";
             price.innerText = '$' + data[i].current_price;
-            priceChange.innerText = data[i].price_change_percentage_24h.toFixed(2) + '%'; 
+            const pChange = data[i].price_change_percentage_24h
+            priceChange.innerHTML = pChange.toFixed(2) + '%'; 
             volume.innerText = '$' + data [i].total_volume.toLocaleString('en-US');
             marketCap.innerText = '$' + data [i].market_cap.toLocaleString('en-US');
-            
+            priceChange.style.color = (pChange >= 0 ? 'green':'red')
             }
         }
     )
      
-    function textColor(){
-        const table = document.getElementById("table");
-        const tr = table.getElementsByTagName("tr");
 
-        for(i = 0; i < tr.length; i++){
-            const priceChange = tr[i].getElementsByClassName("pnum")[0];
-            if (priceChange) {
-                const txtValue = priceChange.textContent;
-                if((txtValue) >=0){
-                    txtValue[i].style.color = "green"
-                } else {
-                    txtValue[i].style.color = "red"
-                }
-            }
-        }
-    }  
-    textColor()
+   
+    
     function findCrypto(){
         const input = document.getElementById("input");
         const filter = input.value.toUpperCase();
